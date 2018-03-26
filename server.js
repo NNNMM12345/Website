3221b63fa25c96dc0c6ea24325cf4a6c2feaa8c7
Miles <milesdpratt@gmail.com>
2018-03-26 12:50:35 -0400

    Build server to serve static HTML. Connect MongoDB for user auth.

---
+++ server.js
@@ -0,0 +1,60 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+const bodyParser = require("body-parser");
+const express = require("express");
+const flash = require("connect-flash");
+const LocalStrategy = require("passport-local");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// database === === ===
+mongoose.connect(process.env.DATABASE_URL);
+
+// models
+const User = require("./models/user");
+
+// express server
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.locals.moment = require("moment");
+
+// passport auth
+app.use(
+	require("express-session")({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// routes and routers
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/users");
+
+app.use((req, res, next) => {
+	res.locals.user = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
+app.use("/", indexRouter);
+app.use("/users", userRouter);
+
+// start the server
+app.listen(process.env.PORT || 5000, process.env.IP, () => {
+	console.log("Express Server has Started!");
+});
